6f0488321e2440864d235fb75a6c35db951b68e0
Aditya Nigam <nigamadi2209@gmail.com>
2022-04-16 02:24:08 +0530

    Revert "with errors"
    This reverts commit 85c79a97a10dbdea0cfc95f2c7ca6ce6a253adab.

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,24 +1,22 @@
-import React, {useContext, useEffect} from 'react'
+import React, { useContext } from 'react'
 import noteContext from "../context/notes/noteContext"
 import Noteitem from './Noteitem';
 import AddNote from './AddNote';
 
+
 const Notes = () => {
     const context = useContext(noteContext);
-    const {notes, getNotes} = context;
-    useEffect(() => {
-        getNotes()
-    }, [])
+    const {notes} = context;
     return (
         <>
-        <AddNote/>
-        <div className="row my-3">
-            <h2>You Notes</h2> 
-            {notes.map((note)=>{
-                return <Noteitem key={note._id} note={note}/>  
-            })}
+         <AddNote/>
+            <div className="row my-3">
+                <h2>You Notes</h2>
+                {notes.map((note) => {
+                    return <Noteitem key={note._id} note={note} />
+                })}
             </div>
-            </>
+        </>
     )
 }
 
