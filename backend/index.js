5ab03d0a3d9c5df98e5fcbf4fc92f61d27415a36
Aditya Nigam <nigamadi2209@gmail.com>
2022-04-16 18:27:58 +0530

    before connecting to db

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const connectToMongo = require('./db');
 const express = require('express')
+var cors = require('cors') 
 
 connectToMongo();
 const app = express()
 const port = 5000
+
+app.use(cors())
 app.use(express.json())
 
 // Available Routes
@@ -13,4 +16,4 @@ app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`iNotes backend listening at http://localhost:${port}`)
-}) 
+})
