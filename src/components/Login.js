a1dbf71745e6d0612b35cf50c3a12631353be06c
Aditya Nigam <nigamadi2209@gmail.com>
2022-04-27 01:48:15 +0530

    Revert "adding notes no working"
    This reverts commit 2c019325ac72b68a7de6482de5f41afa8ed9ac0f.

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,8 +20,8 @@ const Login = (props) => {
         if (json.success) {
             // Save the auth token and redirect
             localStorage.setItem('token', json.authtoken);
-            props.showAlert("Logged in Successfully", "success")
             history.push("/");
+            props.showAlert("Logged in Successfully", "success")
 
         }
         else {
